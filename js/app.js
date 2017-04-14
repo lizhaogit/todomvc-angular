21cf3b0ffb6cb47542bd75f21809649f25c5fb8c
unknown <>
2017-04-14 21:32:15 +0800

    '把数据按照指定的状态循环到列表'

--- js/app.js
+++ js/app.js
@@ -1,6 +1,29 @@
-(function (window) {
-	'use strict';
+(function(angular) {
+    'use strict';
 
-	// Your starting point. Enjoy the ride!
+    /**
+     * myApp Module
+     * 创建一个新的模板
+     * Description
+     */
+    var myApp = angular.module('myApp', []);
 
-})(window);
+
+    //创建一个控制器
+    myApp.controller('TodomvcController', ['$scope', function($scope) {
+
+        //输入框初始化值
+        $scope.text = '';
+
+        //初始化列表数据
+        $scope.todos = [
+            { id: 1, text: '吃饭', completed: true },
+            { id: 2, text: '睡觉', completed: true },
+            { id: 3, text: '上学', completed: false },
+            { id: 4, text: '打游戏', completed: false }
+        ];
+
+    }]);
+
+
+})(angular);
